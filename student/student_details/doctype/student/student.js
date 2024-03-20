1d2c22b0d9454eaa36d671c5b2f029d7b85ee5a7
Priyanshu Awasthi <priyanshu.awas011@gmail.com>
2024-03-20 10:03:07 +0530

    Frappe_Assignment

---
+++ student/student_details/doctype/student/student.js
@@ -0,0 +1,7 @@
+// Copyright (c) 2024, Priyanshu Awasthi and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Student", {
+// 	refresh(frm) {
+// 	},
+// });
